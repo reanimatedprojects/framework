efa3c4f363d56083ae095bb1a9a861571dc21d85
Simon Amor <simon@leaky.org>
2011-08-20 01:50:11 +0100

    Perl modules and installation instructions
    * Added the essential lib/RPG.pm as generated by Dancer
    * Created a Bundle file for easy installation of some
      required Perl modules
    * Created installation instructions

---
+++ lib/Bundle/RPGWNN.pm
@@ -0,0 +1,34 @@
+package Bundle::RPGWNN;
+
+$VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::RPGWNN - Modules that need to be installed for RPGWNN
+
+=head1 SYNOPSIS
+
+perl -MCPAN -e 'install Bundle::RPGWNN'
+
+=head1 CONTENTS
+
+Dancer 1.3071
+
+Plack 0.9982
+
+Template 2.22
+
+YAML::Any 0.72
+
+=head1 AUTHOR
+
+Simon Amor E<lt>simon@rpgwnn.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2011 Reanimated Projects and Games Ltd
+
