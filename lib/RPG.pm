7c4d27a01d583bf4e0eeaa90f7094a3772ecf3e4
Simon Amor <simon@leaky.org>
2011-09-02 00:09:22 +0100

    Added account registration pages
    * Updated header with extra link, amended css for header
    * Added first registration page (pick an authentication method)
    * Added registration page for local accounts (non-functioning)

--- lib/RPG.pm
+++ lib/RPG.pm
@@ -7,4 +7,33 @@ get '/' => sub {
     template 'index';
 };
 
+# Register a new account
+get '/register' => sub {
+    # Blank out the account registration information so
+    # the user starts again if they visit the /register url
+    session "register" => undef;
+    template "register";
+};
+
+post '/register' => sub {
+    # Validate the form values from the first page
+
+    # Only value is the authentication method (auth_type)
+    my $auth = param("auth_type");
+    if ($auth && $auth eq "local") {
+        return redirect '/register/local';
+    }
+    template "register";
+};
+
+get '/register/local' => sub {
+    # Local account creation only.
+    template "register_local";
+};
+
+post '/register/local' => sub {
+    # Local account creation only.
+    template "register_local";
+};
+
 true;
