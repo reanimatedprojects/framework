fdea659ccba4caac2738a2ab610c299a99eda46c
Simon Amor <simon@leaky.org>
2011-09-08 23:26:04 +0100

    Reg'n system and Expires header for static content
    * Updated the registration page to handle multiple authentication
      methods taken from the config.yml
    * Add Expires/Cache-Control headers to static content to allow
      browsers to cache them. Default is 1 day (86400 seconds).

--- lib/RPG.pm
+++ lib/RPG.pm
@@ -1,5 +1,6 @@
 package RPG;
 use Dancer ':syntax';
+use POSIX;
 
 our $VERSION = '0.1';
 
@@ -12,18 +13,32 @@ get '/register' => sub {
     # Blank out the account registration information so
     # the user starts again if they visit the /register url
     session "register" => undef;
-    template "register";
+
+    my @auth_keys = keys %{config->{ rpgwnn }{ auths }};
+    if (scalar(@auth_keys) == 1) {
+        my $auth_key = shift @auth_keys;
+        return redirect "/register/" . $auth_key;
+    }
+    template "register" => { auths => config->{ rpgwnn }{ auths } };
 };
 
 post '/register' => sub {
     # Validate the form values from the first page
 
     # Only value is the authentication method (auth_type)
-    my $auth = param("auth_type");
-    if ($auth && $auth eq "local") {
-        return redirect '/register/local';
+    my $auth_param = param("auth_type");
+    # If no auth parameter was provided, just show the template page
+    unless ($auth_param) {
+        template "register" => { auths => config->{ rpgwnn }{ auths } }
     }
-    template "register";
+
+    # Extract the auth information
+    my $auth_info = config->{ rpgwnn }{ auths }{ $auth_param };
+    if ($auth_info && $auth_info) {
+        # Redirect to the relevant registration page
+        return redirect "/register/" . $auth_param;
+    }
+    template "register" => { auths => config->{ rpgwnn }{ auths } };
 };
 
 get '/register/local' => sub {
@@ -34,6 +49,20 @@ get '/register/local' => sub {
 post '/register/local' => sub {
     # Local account creation only.
     template "register_local";
+};
+
+# Define any hooks here
+
+# Add Expires and Cache-Control headers to static content (css, images etc)
+# Set it for one day from now.
+hook after_file_render => sub {
+    my $response = shift;
+    my $cache_age = 86400;
+
+    $response->header( "Cache-Control" => "max-age=$cache_age" );
+    $response->header( "Expires" => POSIX::strftime(
+        "%a, %d %b %Y %H:%M:%S GMT", gmtime( time() + $cache_age ) ) );
+    return $response;
 };
 
 true;
