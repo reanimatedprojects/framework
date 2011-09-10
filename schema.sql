73fe0325e7627172358a77fd78c1fa9515c91bbb
Simon Amor <simon@leaky.org>
2011-09-11 00:27:17 +0100

    Updated INSTALL file and added database schema
    The database schema only includes the accounts and
    account_auths tables at present and these are liable
    to change as the account registration process is
    finalised.

---
+++ schema.sql
@@ -0,0 +1,25 @@
+/* Authentication should always be possible via password (assuming user
+ * has created a local password). Other options for the future include
+ * OAuth2.0 or OpenID
+ */
+CREATE TABLE accounts (
+    account_id  INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    email       CHAR(128),
+    PRIMARY KEY(account_id)
+);
+
+/* Not sure how to handle this - we should have some kind of username
+ * for all methods probably, except that makes things more complicated
+ * for anything other than user/pass auth.
+ * 
+ * auth_type should be something like 'local', 'google', 'openid', 'facebook'
+ */
+CREATE TABLE account_auths (
+    account_auth_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    account_id INT(10) UNSIGNED NOT NULL,
+    auth_type CHAR(10) NOT NULL,
+    auth_params MEDIUMBLOB,
+    PRIMARY KEY (account_auth_id),
+    KEY account_id (account_id)
+);
+
