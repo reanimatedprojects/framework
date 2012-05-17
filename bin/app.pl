da08f157ca0f2533fdf6591c07480e878becf2f5
Simon Amor <simon@leaky.org>
2012-05-18 00:05:20 +0100

    Updated source to include GPLv3 header

--- bin/app.pl
+++ bin/app.pl
@@ -1,4 +1,26 @@
 #!/usr/bin/env perl
+
+# RPGWNN - Perl Browser-Based MMORPG Framework
+# Copyright (C) 2011-2012 Reanimated Projects and Games Ltd
+
+# This file is part of RPGWNN
+#
+# RPGWNN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RPGWNN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RPGWNN.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact information for Reanimated Projects and Games Ltd
+# can be found at http://rpgwnn.com/
+
 use Dancer;
 use RPG::App;
 dance;
--- lib/page/register_local.pl
+++ lib/page/register_local.pl
@@ -1,3 +1,30 @@
+# RPGWNN - Perl Browser-Based MMORPG Framework
+# Copyright (C) 2011-2012 Reanimated Projects and Games Ltd
+
+# This file is part of RPGWNN
+#
+# RPGWNN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RPGWNN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RPGWNN.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact information for Reanimated Projects and Games Ltd
+# can be found at http://rpgwnn.com/
+
+# This file is not a module, it is simple 'require'd
+# into the main RPG/App.pm
+
+# Any other modules which are needed
+
+use RPG::Utils;
 
 # Local account creation
 get '/register/local' => sub {
