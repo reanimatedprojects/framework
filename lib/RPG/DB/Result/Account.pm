d1c6f058d81e33c9becc1a7292f7fd361d955448
Simon Amor <simon@leaky.org>
2013-05-23 21:58:20 +0100

    Start of the character database module
    Inserting a record into the database manually shows the character
    on the account page.

--- lib/RPG/DB/Result/Account.pm
+++ lib/RPG/DB/Result/Account.pm
@@ -65,6 +65,10 @@ __PACKAGE__->has_many(
     account_auths => 'RPG::DB::Result::AccountAuth',
     'account_id'
 );
+__PACKAGE__->has_many(
+    characters => 'RPG::DB::Result::Character',
+    'account_id'
+);
 
 =head2 new()
 
---
+++ lib/RPG/DB/Result/Character.pm
@@ -0,0 +1,105 @@
+package RPG::DB::Result::Character;
+
+# RPGWNN - Perl Browser-Based MMORPG Framework
+# Copyright (C) 2011-2013 Reanimated Projects and Games Ltd
+
+# This file is part of RPGWNN
+#
+# RPGWNN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RPGWNN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RPGWNN.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact information for Reanimated Projects and Games Ltd
+# can be found at http://rpgwnn.com/
+
+=head1 NAME
+
+RPG::DB::Result::Character - Database module for characters
+
+=head1 DESCRIPTION
+
+This object represents a character - including the name, account id
+and various other character specific parameters that will be added
+as required.
+
+The account id provides a reference into the accounts table which
+allows us to select all characters for the logged in account.
+
+=head1 METHODS
+
+=cut
+
+use base qw/DBIx::Class::Core RPG::DB::Base/;
+
+use Dancer ':syntax';
+
+use strict;
+use warnings;
+
+__PACKAGE__->table("characters");
+__PACKAGE__->add_columns(
+    character_id => {
+        data_type           => "integer",
+        size                => 11,
+        is_auto_increment   => 1,
+        extra               => { unsigned => 1 },
+    },
+    account_id => {
+        data_type           => "integer",
+        size                => 11,
+        is_nullable         => 0,
+        extra               => { unsigned => 1 },
+    },
+    name => {
+        data_type           => "char",
+        size                => 64,
+        is_nullable         => 0,
+    },
+);
+__PACKAGE__->set_primary_key('character_id');
+__PACKAGE__->add_unique_constraint("name" => [qw/name/]);
+__PACKAGE__->belongs_to(
+    account => 'RPG::DB::Result::Account',
+    'account_id'
+);
+
+=head2 new()
+
+We override this method so that we can check the name
+provided is a valid syntax.
+
+=cut
+
+sub new {
+    my ( $class, $attrs ) = @_;
+
+    # FIXME: Check the name is acceptable
+    # FIXME: Check what to return if it's not - what does DBIx::Class do?
+    # FIXME: 5-64 alphanumeric characters
+    #
+    return undef unless ($attrs->{ name } &&
+        $attrs->{ name } =~ /^[a-z0-9A-Z]{5,64}$/);
+
+    my $new = $class->next::method($attrs);
+    return $new;
+}
+
+1;
+
+=head1 AUTHOR
+
+Simon Amor E<lt>simon@rpgwnn.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2011-2013 Reanimated Projects and Games Ltd
+
