f0a3cfd3fc2c47fea73464dd340adc7289b403ff
Simon Amor <simon@leaky.org>
2013-08-31 01:01:13 +0100

    Beginning of the map modules
    Map locations will be identified either by a unique id or by
    x,y,z and world co-ordinates. Tiles within the same world are
    assumed to be on the same scale, but they don't have to be.
    It allows us to separate inside a building from outside the
    building with an entrance from one tile only (using an 'exit'
    that links to a map id on a different world).

---
+++ lib/RPG/DB/Result/Map.pm
@@ -0,0 +1,101 @@
+package RPG::DB::Result::Map;
+
+# RPGWNN - Perl Browser-Based MMORPG Framework
+# Copyright (C) 2011-2013 Reanimated Projects and Games Ltd
+
+# This file is part of RPGWNN
+#
+# RPGWNN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RPGWNN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RPGWNN.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact information for Reanimated Projects and Games Ltd
+# can be found at http://rpgwnn.com/
+
+=head1 NAME
+
+RPG::DB::Result::Map - Database module for map
+
+=head1 DESCRIPTION
+
+This object represents a map tile - including the name, map id
+and various other map specific parameters that will be added
+as required.
+
+=head1 METHODS
+
+=cut
+
+use base qw/DBIx::Class::Core RPG::DB::Base/;
+
+use RPG::Utils;
+use Dancer ':syntax';
+
+use strict;
+use warnings;
+
+__PACKAGE__->table("maps");
+__PACKAGE__->add_columns(
+    map_id => {
+        data_type           => "integer",
+        size                => 11,
+        is_auto_increment   => 1,
+        extra               => { unsigned => 1 },
+    },
+    x => {
+        data_type           => "integer",
+        size                => 11,
+        is_nullable         => 0,
+    },
+    y => {
+        data_type           => "integer",
+        size                => 11,
+        is_nullable         => 0,
+    },
+    z => {
+        data_type           => "integer",
+        size                => 11,
+        is_nullable         => 0,
+    },
+    world => {
+        data_type           => "integer",
+        size                => 11,
+        is_nullable         => 0,
+    },
+    tile_id => {
+        data_type           => "integer",
+        size                => 10,
+        is_nullable         => 0,
+        extra               => { unsigned => 1 },
+    },
+    name => {
+        data_type           => "char",
+        size                => 32,
+        is_nullable         => 0,
+    },
+);
+__PACKAGE__->set_primary_key('map_id');
+__PACKAGE__->might_have(
+    description => 'RPG::DB::Result::MapDescription',
+    'map_id'
+);
+
+1;
+
+=head1 AUTHOR
+
+Simon Amor E<lt>simon@rpgwnn.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2011-2013 Reanimated Projects and Games Ltd
+
---
+++ lib/RPG/DB/Result/MapDescription.pm
@@ -0,0 +1,74 @@
+package RPG::DB::Result::MapDescription;
+
+# RPGWNN - Perl Browser-Based MMORPG Framework
+# Copyright (C) 2011-2013 Reanimated Projects and Games Ltd
+
+# This file is part of RPGWNN
+#
+# RPGWNN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RPGWNN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RPGWNN.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact information for Reanimated Projects and Games Ltd
+# can be found at http://rpgwnn.com/
+
+=head1 NAME
+
+RPG::DB::Result::Map - Database module for map
+
+=head1 DESCRIPTION
+
+This object represents a map tile - including the name, map id
+and various other map specific parameters that will be added
+as required.
+
+=head1 METHODS
+
+=cut
+
+use base qw/DBIx::Class::Core RPG::DB::Base/;
+
+use RPG::Utils;
+use Dancer ':syntax';
+
+use strict;
+use warnings;
+
+__PACKAGE__->table("map_descriptions");
+__PACKAGE__->add_columns(
+    map_id => {
+        data_type           => "integer",
+        size                => 11,
+        is_auto_increment   => 1,
+        extra               => { unsigned => 1 },
+    },
+    description => {
+        data_type           => "mediumtext",
+        is_nullable         => 0,
+    },
+);
+__PACKAGE__->set_primary_key('map_id');
+__PACKAGE__->belongs_to(
+    map => 'RPG::DB::Result::Map',
+    'map_id'
+);
+
+1;
+
+=head1 AUTHOR
+
+Simon Amor E<lt>simon@rpgwnn.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2011-2013 Reanimated Projects and Games Ltd
+
